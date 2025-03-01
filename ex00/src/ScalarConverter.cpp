c1ca1509e06414424e08933f6ffe118b9aa93450
Simon Dablander <sdabland@student.42vienna.com>
2025-03-01 13:55:20 +0100

    feat(ex00): add `InputType` enum & `detectInputType` function

--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "ScalarConverter.hpp"
 
 ScalarConverter::ScalarConverter() {
@@ -13,6 +15,38 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &) {
     return *this;
 }
 
+enum InputType {
+    InputChar,
+    InputInt,
+    InputFloat,
+    InputDouble,
+    InputInvalid,
+};
+
+// TODO: Maybe trim string?
+static InputType detectInputType(const std::string &input) {
+    if (isCharInput(input)) return InputChar;
+
+    return (InputInvalid);
+}
+
 void ScalarConverter::convert(const std::string &raw) {
-    (void)raw;
+    InputType input_type = detectInputType(raw);
+    switch (input_type) {
+        case InputChar:
+            std::cout << "Input is char" << std::endl;
+            break;
+        case InputInt:
+            std::cout << "Input is int" << std::endl;
+            break;
+        case InputFloat:
+            std::cout << "Input is float" << std::endl;
+            break;
+        case InputDouble:
+            std::cout << "Input is double" << std::endl;
+            break;
+        case InputInvalid:
+            std::cout << "Input is INVALID" << std::endl;
+            break;
+    }
 }
